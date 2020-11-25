e6b4c4792b4c8439261ec2f1a3b6b54735a03cf1
Heniker <art_saif@mail.ru>
2020-11-25 20:56:56 +0300

    .

--- vue-route-builder/buildRoutes.ts
+++ vue-route-builder/buildRoutes.ts
@@ -5,25 +5,36 @@ const traverseBack = (str: string) => {
   return str.replace(/[^.]+(?=$)/g, '').slice(0, -1)
 }
 
+// lodash we deserve:
 const last = (arr: any[]) => arr[arr.length - 1]
 
 const tap = (arg: any, fn: (arg: any) => void) => {
   fn(arg)
+  return arg
+}
+
+const flow = (...arg) => {
+  return (acc) => arg.reduce((val, fn) => fn(val), acc)
 }
 
 export function buildRoutes(
-  weakContext: __WebpackModuleApi.RequireContext,
-  lazyContext: __WebpackModuleApi.RequireContext
+  context: {
+    weak: __WebpackModuleApi.RequireContext
+    lazy: __WebpackModuleApi.RequireContext
+  },
+  { childrenFolder = '_children' }: { childrenFolder?: string } = {}
 ) {
-  const paths = weakContext.keys()
+  const paths = context.weak.keys()
 
   const keysObj: Record<string, Partial<import('vue-router').RouteConfig>> = {}
 
   paths.forEach((it, i) => {
     const str = traverseBack(
       it
+        .replace(/^\./, '')
+        .replace(/@/g, ':')
         .replace(/(?<!^)\/$/g, '')
-        .replace(/(_children)\//g, `.children.[${i}].`)
+        .replace(new RegExp(`(${childrenFolder})/`, 'g'), `.children.[${i}].`)
         .replace('index', '')
     )
 
@@ -31,15 +42,15 @@ export function buildRoutes(
       keysObj,
       str,
       Object.assign(get(keysObj, str) || {}, {
-        component: () => lazyContext(it).then(it => it.default),
-        name: last(str.split('.')) === '/' ? undefined : weakContext.resolve(it),
+        component: () => context.lazy(it).then((it) => it.default),
+        name: last(str.split('.')) === '/' ? undefined : context.weak.resolve(it),
         path: last(str.split('.')),
       })
     )
 
     if (str.includes('children')) {
       set(keysObj, traverseBack(str), get(keysObj, str))
-      tap(get(keysObj, traverseBack(traverseBack(traverseBack(str)))), arg => {
+      tap(get(keysObj, flow(traverseBack, traverseBack, traverseBack), str), (arg) => {
         if (arg.children) {
           arg.children = arg.children.filter(Boolean)
         }
