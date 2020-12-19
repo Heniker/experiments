e66e9ec357aea176e4fb1cb9e6291fa9146686ed
Heniker <art_saif@mail.ru>
2020-12-19 14:12:19 +0300

    .

--- vue-route-builder/buildRoutes.ts
+++ vue-route-builder/buildRoutes.ts
@@ -1,62 +1,64 @@
-import get from 'lodash.get'
 import set from 'lodash.set'
 
-const traverseBack = (str: string) => {
-  return str.replace(/[^.]+(?=$)/g, '').slice(0, -1)
-}
-
-// lodash we deserve:
-const last = (arr: any[]) => arr[arr.length - 1]
-
-const tap = (arg: any, fn: (arg: any) => void) => {
-  fn(arg)
-  return arg
-}
-
-const flow = (...arg) => {
-  return (acc) => arg.reduce((val, fn) => fn(val), acc)
-}
-
-export function buildRoutes(
-  context: {
-    weak: __WebpackModuleApi.RequireContext
-    lazy: __WebpackModuleApi.RequireContext
-  },
-  { childrenFolder = '_children' }: { childrenFolder?: string } = {}
-) {
+const getParsedPaths = (context) => {
   const paths = context.weak.keys()
+  const result = {}
 
-  const keysObj: Record<string, Partial<import('vue-router').RouteConfig>> = {}
+  paths.forEach((it) => {
+    const segments = it
+      .replace(/\.vue$/g, '')
+      .slice(2)
+      .split('/')
 
-  paths.forEach((it, i) => {
-    const str = traverseBack(
-      it
-        .replace(/^\./, '')
-        .replace(/@/g, ':')
-        .replace(/(?<!^)\/$/g, '')
-        .replace(new RegExp(`(${childrenFolder})/`, 'g'), `.children.[${i}].`)
-        .replace('index', '')
-    )
+    set(result, segments, {
+      name: context.weak.resolve(it),
+      component: () => context.lazy(it),
+    })
+  })
+  return result
+}
 
-    set(
-      keysObj,
-      str,
-      Object.assign(get(keysObj, str) || {}, {
-        component: () => context.lazy(it).then((it) => it.default),
-        name: last(str.split('.')) === '/' ? undefined : context.weak.resolve(it),
-        path: last(str.split('.')),
-      })
-    )
+const recursiveBuildRoutes = (
+  currentVal: Record<string, any>,
+  result: import('vue-router').RouteConfig[],
+  currentPath: string
+) => {
+  let parent: import('vue-router').RouteConfig = { path: '' }
 
-    if (str.includes('children')) {
-      set(keysObj, traverseBack(str), get(keysObj, str))
-      tap(get(keysObj, flow(traverseBack, traverseBack, traverseBack), str), (arg) => {
-        if (arg.children) {
-          arg.children = arg.children.filter(Boolean)
-        }
-      })
+  if (currentVal.index) {
+    parent = currentVal.index
+    parent.path = currentPath
+    result.push(parent)
+    delete currentVal.index
+  }
+
+  if (currentVal._children) {
+    parent.children = parent.children || []
+
+    delete parent.name
+
+    recursiveBuildRoutes(currentVal._children, parent.children, '')
+
+    delete currentVal._children
+  }
+
+  Object.entries(currentVal).forEach(([key, val]) => {
+    if (val.index) {
+      recursiveBuildRoutes(val, result, `${currentPath + key}/`)
+    } else {
+      val.path = key
+      result.push(val)
     }
   })
 
-  return Object.entries(keysObj).map(([key, val]) => Object.assign(val, { path: key }))
+  return result
+}
+
+export function buildRoutes(context: {
+  weak: __WebpackModuleApi.RequireContext
+  lazy: __WebpackModuleApi.RequireContext
+}) {
+  const result = recursiveBuildRoutes(getParsedPaths(context), [], '/')
+
+  return result
 }
