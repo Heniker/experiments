1e21d73599c8d82683f8203c3c037b8e801ebe13
Heniker <art_saif@mail.ru>
2021-03-12 10:08:05 +0300

    Update PoorMansObservable.ts

--- PoorMansObservable.ts
+++ PoorMansObservable.ts
@@ -1,41 +1,89 @@
-function disallowConcurrency(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-  const originalFn = descriptor.value
-  let inprogressPromise = Promise.resolve()
-  descriptor.value = async function (...args: unknown[]) {
-    await inprogressPromise
-    inprogressPromise = inprogressPromise.then(() => originalFn.bind(this)(...args))
-    return inprogressPromise
-  }
-}
+// prolly the most needless usage of WeakRef
 
 export class Observable<T extends unknown> {
-  private resolve!: (arg: T) => void
+  private resolve: (arg: T) => void = () => {}
 
-  private promise = new Promise<T>((resolve) => {
-    this.resolve = resolve
+  private promises: Set<WeakRef<Promise<T>>> = new Set()
+
+  emitCount = 0
+
+  finalizationGroup = new FinalizationRegistry((ref) => {
+    this.promises.delete(ref)
+    this.emitCount--
   })
 
-  private isActive = false
-
-  @disallowConcurrency
-  private async updatePromise(arg: T) {
-    this.resolve(arg)
-    await this.promise
-    this.promise = new Promise<T>((resolve) => {
-      this.resolve = resolve
-    })
+  constructor() {
+    this.updatePromise(undefined as any)
   }
 
-  emit(arg: T) {
-    if (this.isActive) {
-      void this.updatePromise(arg)
-    }
+  private updatePromise(arg: T) {
+    this.resolve(arg)
+
+    const promise = new Promise<T>((resolve) => {
+      this.resolve = resolve
+    })
+    const ref = new WeakRef(promise)
+
+    this.finalizationGroup.register(promise, ref)
+    this.promises.add(ref)
   }
 
   async *values() {
-    this.isActive = true
+    const iterator = this.promises.values()
+
+    ;[...new Array(this.emitCount)].forEach(() => {
+      iterator.next()
+    })
+
     while (true) {
-      yield await this.promise
+      yield await iterator.next().value.deref()
     }
   }
+
+  [Symbol.asyncIterator] = this.values
+
+  emit(arg: T) {
+    this.emitCount++
+    void this.updatePromise(arg)
+  }
 }
+
+// usage:
+
+const obs = new Observable<string>()
+
+obs.emit('ignored')
+;(async () => {
+  for await (let it of obs) {
+    console.log(it)
+  }
+})()
+;(async () => {
+  const vals = obs.values()
+
+  console.log((await vals.next()).value)
+
+  await new Promise((res) => setTimeout(res, 2000))
+
+  console.log((await vals.next()).value)
+  console.log((await vals.next()).value)
+})()
+
+setTimeout(() => {
+  obs.emit('43')
+}, 1400)
+setTimeout(() => {
+  obs.emit('44')
+}, 1500)
+setTimeout(() => {
+  obs.emit('45')
+}, 1600)
+
+/*
+43
+43
+44
+45
+44
+45
+*/
